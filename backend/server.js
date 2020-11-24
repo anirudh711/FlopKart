a7b3b4ff8cf61e49954ab728b355504c614210a1
Anirudh Madhavan <anirudhragavender@gmail.com>
2020-11-24 20:00:46 +0530

    backend upload multer

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import path from 'path'
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import {notFound,errorHandler} from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes'
 const app =express();
 app.use(express.json())
 dotenv.config()
@@ -16,8 +18,11 @@ app.get('/',async(req,res)=>{
 app.use('/auth/users',userRoutes)
 app.use('/api/products',productRoutes)
 app.use('/api/orders',orderRoutes)
+app.use('/api/upload',uploadRoutes)
 
 app.get('/api/config/paypal',async(req,res)=>res.send(process.env.PAYPAL_CLIENT_ID))
+const __dirname=path.resolve();
+app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
 app.use(notFound);
 app.use(errorHandler)
 const PORT=process.env.PORT || 5000;
