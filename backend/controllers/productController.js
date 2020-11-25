30377cc0fe84a043a23d733a0f6e8ff98d0028f6
Anirudh Madhavan <anirudhragavender@gmail.com>
2020-11-25 15:55:06 +0530

    backend product review

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -77,4 +77,35 @@ const updateProduct = asyncHandler(async (req, res) => {
   res.json(updatedProduct);
 });
 
-export { getProductById, getProducts, deleteProduct,createProduct,updateProduct };
+//@route PUT /api/products/:id/reviews
+//@desc Create new Review
+//@access Private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating,comment } = req.body;
+  const product =await Product.findById(req.params.id)
+  if(product){
+    const alreadyReviewed =product.reviews.find(r=>r.user.toString()==req.user._id.toString())
+    if(alreadyReviewed){0
+      res.status(400)
+      throw new Error('Product already reviewed')
+    }
+    const review={
+      name:req.user.name,
+      rating:Number(rating),
+      comment,
+      user:req.user._id
+    }
+    product.reviews.push(review)
+    product.numReviews=product.reviews.length
+    product.rating=product.reviews.reduce((acc,item)=>item.rating+acc,0)/product.reviews.length;
+    await product.save();
+    res.status(201).json({message:'Review added'})
+  }else{
+      res.status(404);
+      throw new Error('Product not found')
+  }
+  const updatedProduct = await product.save();
+  res.json(updatedProduct);
+});
+
+export { getProductById, getProducts, deleteProduct,createProduct,updateProduct,createProductReview };
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path'
 import dotenv from 'dotenv';
+import morgan from 'morgan'
 import connectDB from './config/db.js';
 import {notFound,errorHandler} from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
@@ -8,6 +9,10 @@ import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 const app =express();
+
+if(process.env.NODE_ENV==='development'){
+    app.use(morgan('dev'))
+}
 app.use(express.json())
 dotenv.config()
 connectDB();
