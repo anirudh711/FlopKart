b49c74fc8075fe43a2951c2990f98f8d3b522b1e
Anirudh Madhavan <anirudhragavender@gmail.com>
2020-11-25 23:57:57 +0530

    pagination bug fix- issue done

--- frontend/src/components/Paginate.js
+++ frontend/src/components/Paginate.js
@@ -2,15 +2,29 @@ import React from "react";
 import { Pagination } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 const Paginate = ({ pages, page, isAdmin = "false", keyword = "" }) => {
-  return pages>1 && (
+  return (
+    pages > 1 && (
       <Pagination>
-          {[...Array(pages).keys()].map(x=>(
-              <LinkContainer key={x+1} to={!isAdmin?keyword?`/search/${keyword}/page/${x+1}`:`/page/${x+1}`:`/admin/productlist/${x+1}`}>
-                  <Pagination.Item active={x+1===page}>{x+1}</Pagination.Item>
-              </LinkContainer>
-          ))}
+        {[...Array(pages).keys()].map((x) => (
+          <LinkContainer
+            key={x + 1}
+            to={
+              !isAdmin
+                ? keyword
+                  ? `/search/${keyword}/page/${x + 1}`
+                  : `/page/${x + 1}`
+                : `/admin/productlist/${x + 1}`
+            }
+          >
+            <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
+          </LinkContainer>
+        ))}
       </Pagination>
-  )
+    )
+  );
 };
+Paginate.defaultProps={
+    isAdmin:false
+}
 
 export default Paginate;
